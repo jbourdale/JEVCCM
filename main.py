1fa9e8f7b5efa9ca8189f1c86a57b199d934017d
Jules Bourdalé <jules.bourdale@gmail.com>
2017-05-22 01:43:46 +0200

    Update main.py
    main.py : Removing useless comment

--- main.py
+++ main.py
@@ -20,29 +20,22 @@ if __name__ == "__main__":
             name = sys.argv[3]
         except e:
             exit()
-    moves = ['N', 'S', 'E', 'O']
-    i = 0
 
     net = network.Network(ip, port, name)
-    # print("Numero de joueur : "+str(net.getNumPlayer()))
 
     mouleFixed = None
     while 1:
-        # print("RECEPTION DU PLATEAU ")
         game = net.getBoardState()
 
         if not game:
             break
 
         myself = game.players[net.getNumPlayer()]
-        # print("COMPUTING DE L'IA")
         depart = game.getPlayerCase(myself)
 
 
         if mouleFixed and type(game.getCase(mouleFixed.y, mouleFixed.x)) != Callais.Moule:
-            # print("MA MOULE ELLE SES BARRE LA PUTE")
             mouleFixed = None
-        # goal = #IA.find_goal(depart,game.getMoule(),game.listeDepartOthers(myself,game.players),game)
         if not mouleFixed:
             goal = IA.sortMoule(game.getMoule(),depart)[0]
             mouleFixed = goal
@@ -55,8 +48,4 @@ if __name__ == "__main__":
             move = IA.shorter_way(depart,goal,game)[0]
 
 
-
-        # print("ENVOI DU MOVE")
-
         net._send(move)
-        i+=1
